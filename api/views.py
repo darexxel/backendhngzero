6077f90ad31d0f409bafeede5e3484c5c97e7826
darexxel <darexxel@gmail.com>
2025-01-29 20:28:53 +0100

    Update GitHub URL with .git extension in get_info view

--- api/views.py
+++ api/views.py
@@ -6,12 +6,12 @@ from django.utils import timezone
 from datetime import datetime
 
 def get_info(request):
-    # Get current UTC time and format it properly
+    # Get current UTC time
     current_time = timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ')
     
     data = {
         "email": "daryjoe765@gmail.com",
-        "current_datetime": current_time,
-        "github_url": "https://github.com/darad124/hng_stage0"
+        "current_datetime": current_time,  # Dynamic UTC time
+        "github_url": "https://github.com/darad124/hng_stage0.git"  # Added .git
     }
     return JsonResponse(data)
