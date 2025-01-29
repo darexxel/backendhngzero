e07016e7020b301cabf3bbc81cdb735a723fe0c6
darexxel <darexxel@gmail.com>
2025-01-29 21:06:25 +0100

    Remove .git extension from GitHub URL in get_info view

--- api/views.py
+++ api/views.py
@@ -10,6 +10,6 @@ def get_info(request):
     data = {
         "email": "daryjoe765@gmail.com",
         "current_datetime": current_time,  # Dynamic UTC time
-        "github_url": "https://github.com/darad124/hng_stage0.git"  # Added .git
+        "github_url": "https://github.com/darad124/hng_stage0"  # Removed .git
     }
     return JsonResponse(data)
