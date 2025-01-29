55a276820fdb51cd5ad8bc6009f026d308e39fbd
darexxel <darexxel@gmail.com>
2025-01-29 20:10:44 +0100

    Update datetime formatting in get_info view
    - Use timezone.now() with strftime to generate ISO 8601 formatted UTC time
    - Remove unnecessary datetime import

--- api/views.py
+++ api/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from django.utils import timezone
+from datetime import datetime
 
 def get_info(request):
+    # Get current UTC time and format it properly
+    current_time = timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    
     data = {
         "email": "daryjoe765@gmail.com",
-        "current_datetime": timezone.now().isoformat(),
+        "current_datetime": current_time,
         "github_url": "https://github.com/darad124/hng_stage0"
     }
     return JsonResponse(data)
